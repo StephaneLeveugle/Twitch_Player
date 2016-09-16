5dcf4041edccc4e094218f81110172ceede17b56
Stephane Leveugle <stephane.leveugle@gmail.com>
2016-09-16 11:20:32 +0200

    v0.1

--- index.js
+++ index.js
@@ -1,30 +1,203 @@
 $(function(){
+
+  $(document).foundation();
   
-  // let options = {
-  //   width: 854,
-  //   height: 480,
-  //   channel: "ESL_SC2"
-  // };
+  let streamColumnWidth = $(".streamColumn").width(),
+      streamColumnHeight = $(".streamColumn").height();
+
+  // set player to 80% width 
+  let playerOptions = {
+    width: $(".streamColumn").width() * 0.8
+  };
+
+  let initialViewport = getViewport();
+
+  // small - medium screens
+  if(initialViewport.width < 1024) {
+    $(".accordion").css("min-height", 0);
+    // player height to 80%
+    playerOptions.height = $(".streamColumn").height() * 0.8;
+  } 
+  // large screens
+  else {
+    $(".accordion").css("min-height", initialViewport.height);
+    // player height to 60%
+    playerOptions.height = $(".streamColumn").height() * 0.6;
+    console.log(playerOptions.height);
+  }
+
+
+  let player = new Twitch.Player("playerContainer", playerOptions);
+  // pause it not to let it buffer endlessly
+  player.pause();
   
-  // let player = new Twitch.Player("lala", options);
-  // player.setVolume(0.5);
-  // player.addEventListener(Twitch.Player.PAUSE, () => { console.log("player is paused")});
+  let channels = ["ESL_CSGO", "ZeratoR", "Trick2g", "ESL_Overwatch", "OgamingSC2", "cretetion", "FreeCodeCamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
   
-   $.ajax({
+
+  // iterate through the channels array
+  channels.forEach(function(channel, index) {
+    $.ajax({
     
-    url: "https://api.twitch.tv/kraken/streams/ESL_SC2",
-    method: "GET",
-    dataType: "jsonp",
-    headers: {
-      "Client-ID" : "dzcx6bswtv8hnlx6otboezlo19x1m6m"
-    },
-    success: function(response) {
-      $("#test2").text(JSON.stringify(response));
-    }, 
-    error: function(err) {
-      $("#test2").text(JSON.stringify(err))
+      url: "https://api.twitch.tv/kraken/streams/" + channel,
+      method: "GET",
+      dataType: "json",
+      headers: {
+        "Client-ID" : "dzcx6bswtv8hnlx6otboezlo19x1m6m"
+      },
+      success: function(response) {
+
+        // if the stream exists and is online
+        if(response && response.stream && response.stream.channel) {
+
+          console.log(response);
+
+          let html = '<li class="accordion-item" data-accordion-item>' +
+                        '<a href="#" class="accordion-title">' +
+                          '<img class="logo" src="' + response.stream.channel.logo + '" alt="' + channel + ' logo"><span>' + channel +
+                        '</span></a>' +
+                        '<div class="accordion-content" data-tab-content>' +
+                          '<a class="block text-center" href="https://www.twitch.tv/' + channel + '" target="_blank">' +
+                            '<span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" tabindex="1" title="Click here to be redirected to the channel of ' + channel + '">' +
+                              response.stream.channel.status +
+                            '</span>' +
+                          '</a>';
+
+          // if there is a game listed add it
+          if(response.stream.game) {
+            html += '<a class="block text-center" href="https://www.twitch.tv/directory/game/' + response.stream.game + '" target="_blank">' + "Game : " + 
+                      '<span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" tabindex="1" title="Click here to see other people streaming ' + response.stream.game + '">' +
+                        response.stream.game +
+                      '</span>' +
+                    '</a>';
+          }
+
+          // add viewers count
+          html += '<p class="text-center">' + response.stream.viewers + ' people currently watching!</p>';
+
+          // add watch button
+          html += '<button class="block button watchBtn expanded" data-channel="' + channel + '">Watch</button>'
+
+          // close 
+          html += '</div></li>';
+
+          $("#all > .accordion").append(html);
+          $("#online > .accordion").append(html);
+        }
+        // channel doesn't exist
+        else if (response.error) {
+
+          let html = '<li class="accordion-item" data-accordion-item><a href="#" class="accordion-title">' 
+            + response.message 
+            + '</a><div class="accordion-content" data-tab-content>' + response.error + '</div></li>';
+          $("#all > .accordion").append(html);
+          $("#offline > .accordion").append(html);
+        } 
+        // channel offline
+        else {
+
+          let html = '<li class="accordion-item" data-accordion-item><a href="#" class="accordion-title">'
+            + '<img class="logo" src="./resources/svgs/fi-torso.svg" alt="Default Logo for offline channels"><span>' + channel + '</span></a>' 
+            +  '<div class="accordion-content" data-tab-content>'
+            +     '<a href="https://www.twitch.tv/' + channel + '" target="_blank">' + channel + ' is offline</a>'
+            +   '</div></li>';
+
+          $("#all > .accordion").append(html);
+          $("#offline > .accordion").append(html);
+        }
+        
+
+        // re initialize the sidebar
+        Foundation.reInit("accordion");
+
+      }, 
+      error: function(err) {
+        console.log(err);
+        let html = '<li class="accordion-item" data-accordion-item><a href="#" class="accordion-title">' 
+          + err.responseJSON.message 
+          + '</a><div class="accordion-content" data-tab-content>' + err.responseJSON.error + '</div></li>';
+        $("#all > .accordion").append(html);
+        $("#offline > .accordion").append(html);
+
+        Foundation.reInit("accordion");
+      }
+      
+    }); 
+
+    
+
+  });
+
+
+
+  // listening on document 
+  // standard syntax doesn't work on dynamically added elements
+  $(document).on("click", ".watchBtn", function(e) {
+
+    console.log("lala");
+
+    e.preventDefault();
+
+    // get the channel the user wants to watch
+    let channel = $(this).attr("data-channel");
+
+    // make sure the user isn't already watching it
+    if(channel !== player.getChannel()) {
+
+      // change channel
+      player.setChannel(channel);
+      player.play();
+      // change text above player
+      $("#currentStream").text("Currently watching " + channel);
+
+      // smooth scroll if mobile/tablet
+      if(getViewport().width < 1024) {
+        $('html, body').animate({
+          scrollTop: $("#playerContainer").offset().top
+        }, 800);
+      }
     }
+
+
+  });
+
+   
+  // window resize event 
+  // we need to manually resize the twitch player 
+  $(window).on("resize", function(e){
+
+    let viewport = getViewport();
+
+    // small - medium screens
+    if(viewport.width < 1024) {
+      // remove 100% height of accordion
+      $(".accordion").css("min-height", 0);
+      // set stream column height to remaining height 
+      // $(".streamColumn").height($(body).width() - $(".accordion").height() - 1);
+      $("#playerContainer > iframe").attr("height", $(".streamColumn").height() * 0.8);
+    } 
+    // large screens
+    else {
+      $(".accordion").css("min-height", viewport.height);
+      // $(".streamColumn").height($(body).width() - 1);
+      $("#playerContainer > iframe").attr("height", $(".streamColumn").height() * 0.6);
+    }
+
+    $("#playerContainer > iframe").attr("width", $(".streamColumn").width() * 0.8);
     
-  }); 
+
+  });
+
   
 });
+
+
+// get actual viewport width/height
+// is consistent with css media queries
+function getViewport() {
+    var container = window, property = "inner";
+    if (!("innerWidth" in window )) {
+       property = "client";
+       container = document.documentElement || document.body;
+    }
+    return { width : container[property + "Width"], height : container[property + "Height"]};
+}
